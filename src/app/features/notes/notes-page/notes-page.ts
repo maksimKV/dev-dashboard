9ffe3b355af4893a3e4f6df71fc718d9fe346dea
Maksim Kanev <maks_kv@yahoo.com>
2025-06-30 17:27:46 +0300

    Added a markdown note editor with live preview and localStorage

--- src/app/features/notes/notes-page/notes-page.ts
+++ src/app/features/notes/notes-page/notes-page.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-notes-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MarkdownModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './notes-page.html',
   styleUrl: './notes-page.scss'
 })
 export class NotesPage {
+  markdown = '';
 
+  constructor() {
+    this.loadNote();
+  }
+
+  saveNote() {
+    localStorage.setItem('markdown-note', this.markdown);
+  }
+
+  loadNote() {
+    this.markdown = localStorage.getItem('markdown-note') || '';
+  }
+
+  clearNote() {
+    this.markdown = '';
+    this.saveNote();
+  }
 }
