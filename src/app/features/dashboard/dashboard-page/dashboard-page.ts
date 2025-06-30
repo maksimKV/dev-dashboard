ab7c7f0797c51eb7fd64c2e88eb863d7a326b821
Maksim Kanev <maks_kv@yahoo.com>
2025-06-30 20:02:21 +0300

    Enhanced dashboard welcome page with Material card, icons

--- src/app/features/dashboard/dashboard-page/dashboard-page.ts
+++ src/app/features/dashboard/dashboard-page/dashboard-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss'
 })
