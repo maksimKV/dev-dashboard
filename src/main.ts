623c193fd24d9f9144e24b203f8e1f1b323ee045
Maksim Kanev <maks_kv@yahoo.com>
2025-06-30 19:57:10 +0300

    Fixed a bug with the highlight library

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+(window as any).hljs = hljs;
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
