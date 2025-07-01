cd5575c7615afbb7b66bac8ac29cd6131fdff3cd
Maksim Kanev <maks_kv@yahoo.com>
2025-07-01 21:05:54 +0300

    Removed service worker references

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 import { MarkdownModule } from 'ngx-markdown';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
-import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 
 const MARKDOWN_PROVIDERS = MarkdownModule.forRoot().providers ?? [];
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(),
     ...MARKDOWN_PROVIDERS,
     provideCharts(withDefaultRegisterables()),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
     provideHttpClient(withInterceptorsFromDi(), withFetch())
   ]
 };
