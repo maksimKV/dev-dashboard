7f7f71d0e866a6ddfe4158d804061f18662dfab6
Maksim Kanev <maks_kv@yahoo.com>
2025-07-01 22:20:59 +0300

    Added import zone.js statement in several files

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
-import 'zone.js';
+import 'zone.js/dist/zone';
 
 const bootstrap = () => bootstrapApplication(App, config);
 
