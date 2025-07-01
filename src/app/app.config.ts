54cc6ee4484b22b6f6548cf27ed4e36101cbd5c8
Maksim Kanev <maks_kv@yahoo.com>
2025-07-01 21:38:22 +0300

    removed hydration entirely out of the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { MarkdownModule } from 'ngx-markdown';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
@@ -11,9 +10,8 @@ const MARKDOWN_PROVIDERS = MarkdownModule.forRoot().providers ?? [];
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // provideClientHydration(), // Removed for NG0908 isolation
-    // ...MARKDOWN_PROVIDERS, // Commented out for NG0908 isolation
-    // provideCharts(withDefaultRegisterables()), // Commented out for NG0908 isolation
-    // provideHttpClient(withInterceptorsFromDi(), withFetch()) // Commented out for NG0908 isolation
+    ...MARKDOWN_PROVIDERS,
+    provideCharts(withDefaultRegisterables()),
+    provideHttpClient(withInterceptorsFromDi(), withFetch())
   ]
 };
